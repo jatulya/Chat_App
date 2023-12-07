dcb214b1318085da1277139a8d7ef6a72c9f6910
J-Atulya <atulyajayakrishnan@gmail.com>
2023-12-08 00:01:04 +0530

    added io connection-> not working

--- App1/client/src/App.js
+++ App1/client/src/App.js
@@ -1,5 +1,8 @@
 import './App.css';
+import io from "socket.io-client"; //for estavblishing connection
 
+//establishing connection
+const socket = io.connect("http://localhost:3000");
 function App() {
   return (
     <div className="App">
